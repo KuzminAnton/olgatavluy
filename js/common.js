106f8b2cd5cbfa496cf1ddc2b0191ceae6554ea4
KuzminAnton <48728912+KuzminAnton@users.noreply.github.com>
2019-04-09 15:21:09 +0300

    Update common.js

--- js/common.js
+++ js/common.js
@@ -7,9 +7,15 @@ jQuery(function($){
 
 var Form = document.querySelector('#form')
 
-Form.addEventListener('submit', function(e){
-    e.preventDefault()
+function handleSubmit(event){
+    event.preventDefault
     
-    // get input value
-})
+    var phone = document.querySelector('#form > input:nth-child(1)').value
+    var message = document.querySelector('#form > input:nth-child(2)').value
+    
+    var link = 'https://sms.ru/sms/send?api_id=F3020000-849F-0968-4E1D-E08B296F0114&to=79781302164&msg=' + phone + message + '&json=1'
+
+}
+
+Form.addEventListener('submit', handleSubmit)
     
