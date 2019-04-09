8b734f108d6111c1ee416d543ffa4942a5f9f896
KuzminAnton <48728912+KuzminAnton@users.noreply.github.com>
2019-04-09 15:12:51 +0300

    Update common.js

--- js/common.js
+++ js/common.js
@@ -5,4 +5,11 @@ jQuery(function($){
     $("#ssn").mask("999-99-9999");
  });
 
+var Form = document.querySelector('#form')
+
+Form.addEventListener('submit', function(e){
+    e.preventDefault()
+    
+    // get input value
+})
     
