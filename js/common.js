53ce4783dbc310cf1042638b92437a3521c9d0d0
AlexOuttaspace <31132818+AlexOuttaspace@users.noreply.github.com>
2019-04-14 22:58:02 +0300

    Update common.js

--- js/common.js
+++ js/common.js
@@ -6,32 +6,34 @@ jQuery(function($){
  });
 
 var Form = document.querySelector('#form')
+Form.addEventListener('submit', handleSubmit)
 
 function handleSubmit(event){
-    event.preventDefault()
-    
-    var phone = document.querySelector('#form > input:nth-child(1)').value
-    var message = document.querySelector('#form > input:nth-child(2)').value
-    
-    var link = 'https://sms.ru/sms/send?api_id=F3020000-849F-0968-4E1D-E08B296F0114&to=79781302164&msg=' + phone + message + '&json=1'
-    // 1. Создаём новый объект XMLHttpRequest
-var xhr = new XMLHttpRequest();
+  event.preventDefault()
+  
+  var phone = document.querySelector('#form > input:nth-child(1)').value
+  var message = document.querySelector('#form > input:nth-child(2)').value
 
-// 2. Конфигурируем его: GET-запрос на URL 'phones.json'
-xhr.open('GET', link, false);
+  var link = 'https://sms.ru/sms/send?api_id=F3020000-849F-0968-4E1D-E08B296F0114&to=79781302164&msg=' + phone + message + '&json=1'
+  // 1. Создаём новый объект XMLHttpRequest
+  var xhr = new XMLHttpRequest();
 
-// 3. Отсылаем запрос
-xhr.send();
+  // 2. Конфигурируем его: GET-запрос на URL 'phones.json'
+  xhr.open('GET', link);
 
-// 4. Если код ответа сервера не 200, то это ошибка
-if (xhr.status != 200) {
-  // обработать ошибку
-  alert( xhr.status + ': ' + xhr.statusText ); // пример вывода: 404: Not Found
-} else {
-  // вывести результат
-  alert( xhr.responseText ); // responseText -- текст ответа.
+  // 3. Отсылаем запрос
+  xhr.send();
+
+  xhr.onreadystatechange = function(){
+    if (xhr.readyState !== 4) return
+
+    // 4. Если код ответа сервера не 200, то это ошибка
+    if (xhr.status != 200) {
+      // обработать ошибку
+      alert( xhr.status + ': ' + xhr.statusText ); // пример вывода: 404: Not Found
+    } else {
+      // вывести результат
+      alert( xhr.responseText ); // responseText -- текст ответа.
+    }
+  }
 }
-}
-
-// Form.addEventListener('submit', handleSubmit())
-    
